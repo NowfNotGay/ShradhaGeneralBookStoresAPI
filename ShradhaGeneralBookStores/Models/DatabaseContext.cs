b9470786c4f3cb790ca9df5e6bd3f7738c378056
NowfNotGay <hotrungnghia2704@gmail.com>
2023-07-15 16:31:16 +0700

    a

--- ShradhaGeneralBookStores/Models/DatabaseContext.cs
+++ ShradhaGeneralBookStores/Models/DatabaseContext.cs
@@ -59,7 +59,7 @@ public partial class DatabaseContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=LAPTOP-9JFHB4R1\\SQLEXPRESS;Database=ShradhaGeneralBookStores;user id=sa;password=1;trusted_connection=true;encrypt=false");
+        => optionsBuilder.UseLazyLoadingProxies().UseSqlServer("Server=LAPTOP-9JFHB4R1\\SQLEXPRESS;Database=ShradhaGeneralBookStores;user id=sa;password=1;trusted_connection=true;encrypt=false");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
