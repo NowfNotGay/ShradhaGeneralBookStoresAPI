443d9a8c6d7853f196ee8a944ba3d388178d7ed3
NowfNotGay <hotrungnghia2704@gmail.com>
2023-07-16 19:36:07 +0700

    ád

---
+++ ShradhaGeneralBookStores/Models/ModelTemp/OrderAPI.cs
@@ -0,0 +1,21 @@
+﻿namespace ShradhaGeneralBookStores.Models.ModelTemp;
+
+public class OrderAPI
+{
+    public int AccountId { get; set; }
+
+    public decimal TotalPrice { get; set; }
+
+    public int StatusId { get; set; }
+
+    public int AddressId { get; set; }
+
+    public int VoucherId { get; set; }
+
+    public int PaymentMethodId { get; set; }
+
+    public List<OrderDetail> listOrderDetail { get; set; } = new List<OrderDetail>();
+
+}
+
+
